db3a2655be64886c85eeb69e193eec68af484601
hugo puillet <hugopuillet@hotmail.fr>
2023-02-07 19:49:05 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css'
+
+function Home(){
+    return(
+        <div className='page-container'>
+            <h1>ACCUEIL</h1>
+            <h3>vos rêves sont entres de bonnes mains(no scam)</h3>
+            <p>Grâce à notre expertise en : étude, UX, conception, design, développement, SEO. Nous pouvons vous aidez a créer des sites web<br/>Notre agence web est capable de répondre à tous vos besoins et d'élaborer une véritable stratégie digitale.</p>
+        </div>
+    )
+}
+
+export default Home;
